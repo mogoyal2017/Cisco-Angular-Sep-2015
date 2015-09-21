2e2b9987260139abe2eca789f3988ea33fec8bb3
tkmagesh <tkmagesh77@gmail.com>
2015-09-21 15:19:26 +0530

    Model View - Problem

--- Day-01/02-Closures/spinner.js
+++ Day-01/02-Closures/spinner.js
@@ -1,7 +1,23 @@
+/*
 create an object "spinner"
 the object should exhibit the following behavior
+*/
+//factory
+function getSpinner(){
+    var count = 0;
+    function up(){
+        return ++count;
+    }
+    function down(){
+        return --count;
+    }
+    return {
+        up : up,
+        down : down
+    }
+}
 
-var spinner = ...
+var spinner = getSpinner();
 
 spinner.up(); //=> 1
 spinner.up(); //=> 2
@@ -13,3 +29,4 @@ spinner.down(); //=> 2
 spinner.down(); //=> 1
 spinner.down(); //=> 0
 spinner.down(); //=> -1
+
